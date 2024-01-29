f15f0eb23ea0229c8f60e7ffd9685db382e68cf0
Greg Pelletier <gjpelletier@users.noreply.github.com>
2024-01-28 20:02:25 -0800

    Add files via upload

---
+++ delta_method.m
@@ -0,0 +1,145 @@
+function result = delta_method(pcov,popt,x_new,f,x,y,alpha)
+
+% - - -
+% Function to calculate the confidence interval and prediction interval
+% for any user-defined regression function using the delta-method
+% as described in Sec 5.1 of the following online statistics lecture:
+% https://jchiquet.github.io/MAP566/docs/regression/map566-lecture-nonlinear-regression.html
+%
+% Greg Pelletier (gjpelletier@gmail.com)
+% - - -
+% INPUT
+% pcov = variance-covariance matrix of the model parameters (e.g. from MATLAB nlinfit)
+% popt = optimum best-fit parameters of the regression function (e.g. from MATLAB nlinfit)
+% x_new = new x values to evaluate new predicted y_new values (e.g. x_new=linspace(min(x),max(x),100)
+% f = user-defined regression @ function to predict y given inputs of parameters and x values (e.g. observed x or x_new)
+% 	For example, if using the 4-parameter sigmoid function, then
+% 	f = @(param,xval) (param(1)-param(4)) ./ (1+exp(-param(2) .* (xval-param(3))))+param(4)
+% x = observed x
+% y = observed y
+% alpha = significance level for the confidence/prediction interval (e.g. alpha=0.05 is the 95% confidence/prediction interval)
+% - - -
+% OUTPUT
+% result = structure with the following output variables:
+%        'popt': optimum best-fit parameter values used as input
+%        'pcov': variance-covariance matrix used as input
+%        'alpha': input significance level for the confidence/prediction interval (e.g. alpha=0.05 is the 95% confidence/prediction interval)
+%        'x': observed x values used as input
+%        'y': observed y values used as input
+%        'yhat': predicted y at observed x values
+%        'x_new': new x-values used as input to evaluate unew predicted y_new values
+%        'y_new': new predicted y_new values at new x_new values
+%        'lwr_conf': lower confidence interval for each value in x_new
+%        'upr_conf': upper confidence interval for each value in x_new
+%        'lwr_pred': lower prediction interval for each value in x_new
+%        'upr_pred': upper prediction interval for each value in x_new
+%        'grad_new': derivative gradients at x_new (change in f(x_new) per change in each popt)
+%        'G_new': variance due to each parameter at x_new
+%        'GS_new': variance due to all parameters combined at x_new
+%        'SST': Sum of Squares Total
+%        'SSR': Sum of Squares Regression
+%        'SSE': Sum of Squares Error
+%        'MSR': Mean Square Regression
+%        'MSE': Mean Square Error of the residuals
+%        'syx': standard error of the estimate
+%        'nobs': number of observations
+%        'nparam': number of parameters
+%        'df': degrees of freedom = nobs-nparam
+%        'qt': 2-tailed t-statistic at alpha
+%        'Fstat': F-statistic = MSR/MSE
+%        'dfn': degrees of freedom for the numerator of the F-test = nparam-1
+%        'dfd': degrees of freedom for the denominator of the F-test = nobs-nparam
+%        'pvalue': signficance level of the regression from the probability of the F-test
+%        'rsquared': r-squared = SSR/SST
+%        'adj_rsquared': adjusted squared
+
+% calculate predicted y_new at each x_new
+y_new = f(popt,x_new);
+% calculate derivative gradients at x_new (change in f(x_new) per change in each popt)
+grad_new = nan(length(x_new),length(popt));
+h = 1e-8;       % h= small change for each popt to balance truncation error and rounding error of the gradient
+for i=1:length(popt)
+	% make a copy of popt
+	popt2 = popt;
+	% gradient forward
+	popt2(i) = (1+h) * popt(i);
+	y_new2 = f(popt2, x_new);
+	dy = y_new2 - y_new;
+	dpopt = popt2(i) - popt(i);
+	grad_up = dy / dpopt;
+	% gradient backward
+	popt2(i) = (1-h) * popt(i);
+	y_new2 = f(popt2, x_new);
+	dy = y_new2 - y_new;
+	dpopt = popt2(i) - popt(i);
+	grad_dn = dy / dpopt;
+	% centered gradient is the average gradient forward and backward
+	grad_new(:,i) = (grad_up + grad_dn) ./ 2;
+end
+% calculate variance in y_new due to each parameter and for all parameters combined
+G_new = (grad_new * pcov) .* grad_new;    	% variance in y_new due to each popt at each x_new
+GS_new = sum(G_new,2);                 		% total variance from all popt values at each x_new
+% - - -
+% lwr_conf and upr_conf are confidence intervals of the best-fit curve
+nobs = length(x);
+nparam = length(popt);
+df = nobs - nparam;
+qt = tinv(1-alpha/2, df);
+delta_f = sqrt(GS_new) .* qt;
+lwr_conf = y_new' - delta_f;
+upr_conf = y_new' + delta_f;
+% lwr_pred and upr_pred are prediction intervals of new observations
+yhat = f(popt,x);
+SSE = sum((y-yhat) .^ 2);                 % sum of squares (residual error)
+MSE = SSE ./ df;                          % mean square (residual error)
+syx = sqrt(MSE);                          % std error of the estimate
+delta_y = sqrt(GS_new + MSE) .* qt;
+lwr_pred = y_new' - delta_y;
+upr_pred = y_new' + delta_y;
+% - - -
+% optional additional outputs of regression statistics
+SST = sum(y .^ 2) - sum(y) .^ 2 ./ nobs;  	% sum of squares (total)
+SSR = SST - SSE;                            % sum of squares (regression model)
+MSR = SSR / (length(popt)-1);              	% mean square (regression model)
+Fstat = MSR / MSE;           				% F statistic
+dfn = length(popt) - 1;    					% df numerator = degrees of freedom for model = number of model parameters - 1
+dfd = df;                    				% df denomenator = degrees of freedom of the residual = df = nobs - nparam
+% pvalue = 1-stats.f.cdf(Fstat, dfn, dfd);    % p-value of F test statistic
+pvalue = 1 - fcdf(Fstat, dfn, dfd);			% p-value of F test statistic 
+rsquared = SSR / SST;                                                        	% ordinary rsquared
+adj_rsquared = 1-(1-rsquared) .* (length(x)-1) ./ (length(x)-length(popt)-1);  	% adjusted rsquared
+% - - -
+% put result into an output structure
+result = [];
+result.popt = popt;
+result.pcov = pcov;
+result.alpha = alpha;
+result.x_new = x_new';
+result.y_new = y_new';
+result.lwr_conf = lwr_conf;
+result.upr_conf = upr_conf;
+result.lwr_pred = lwr_pred;
+result.upr_pred = upr_pred;
+result.grad_new = grad_new;
+result.G_new = G_new;
+result.GS_new = GS_new;
+result.x = x';
+result.y = x';
+result.yhat = yhat';
+result.nobs = nobs;
+result.nparam = nparam;
+result.SSE = SSE;
+result.MSE = MSE;
+result.syx = syx;
+result.SST = SST;
+result.SSR = SSR;
+result.MSR = MSR;
+result.Fstat = Fstat;
+result.qt = qt;
+result.df = df;
+result.dfn = dfn;
+result.dfd = dfd;
+result.pvalue = pvalue;
+result.rsquared = rsquared;
+result.adj_rsquared = adj_rsquared;
+
---
+++ parametric_bootstrap.m
@@ -0,0 +1,145 @@
+function result = parametric_bootstrap(popt,x_new,f,x,y,alpha,trials)
+
+% - - -
+% Function to calculate the confidence interval and prediction interval
+% for any user-defined regression function using the parametric bootstrap method
+% as described in Sec 5.1 of the following online statistics lecture:
+% https://jchiquet.github.io/MAP566/docs/regression/map566-lecture-nonlinear-regression.html
+%
+% Greg Pelletier (gjpelletier@gmail.com)
+% - - -
+% INPUT
+% pcov = variance-covariance matrix of the model parameters (e.g. from MATLAB nlinfit)
+% x_new = new x values to evaluate new predicted y_new values (e.g. x_new=linspace(min(x),max(x),100)
+% f = user-defined regression @ function to predict y given inputs of parameters and x values (e.g. observed x or x_new)
+% 	For example, if using the 4-parameter sigmoid function, then
+% 	f = @(param,xval) (param(1)-param(4)) ./ (1+exp(-param(2) .* (xval-param(3))))+param(4)
+% x = observed x
+% y = observed y
+% alpha = significance level for the confidence/prediction interval (e.g. alpha=0.05 is the 95% confidence/prediction interval)
+% trials = number of iteration trials for Monte Carlo simulation
+% - - -
+% OUTPUT
+% result = structure with the following output variables:
+%        'popt': optimum best-fit parameter values used as input
+%        'alpha': input significance level for the confidence/prediction interval (e.g. alpha=0.05 is the 95% confidence/prediction interval)
+%        'x': observed x values used as input
+%        'y': observed y values used as input
+%        'yhat': predicted y at observed x values
+%        'x_new': new x-values used as input to evaluate unew predicted y_new values
+%        'y_new': new predicted y_new values at new x_new values
+%        'lwr_conf': lower confidence interval for each value in x_new
+%        'upr_conf': upper confidence interval for each value in x_new
+%        'lwr_pred': lower prediction interval for each value in x_new
+%        'upr_pred': upper prediction interval for each value in x_new
+%        'SST': Sum of Squares Total
+%        'SSR': Sum of Squares Regression
+%        'SSE': Sum of Squares Error
+%        'MSR': Mean Square Regression
+%        'MSE': Mean Square Error of the residuals
+%        'syx': standard error of the estimate
+%        'nobs': number of observations
+%        'nparam': number of parameters
+%        'df': degrees of freedom = nobs-nparam
+%        'qt': 2-tailed t-statistic at alpha
+%        'Fstat': F-statistic = MSR/MSE
+%        'dfn': degrees of freedom for the numerator of the F-test = nparam-1
+%        'dfd': degrees of freedom for the denominator of the F-test = nobs-nparam
+%        'pvalue': signficance level of the regression from the probability of the F-test
+%        'rsquared': r-squared = SSR/SST
+%        'adj_rsquared': adjusted squared
+
+% calculate predicted y_new at each x_new
+y_new = f(popt,x_new);
+% some things we need for the bootstrap
+nobs = length(x);
+nparam = length(popt);
+n_new = length(x_new);
+df = nobs - nparam;
+qt = tinv(1-alpha/2, df);
+qnorm = norminv(1-alpha/2);
+rq = qt/qnorm; 							% ratio of t-score to normal-score for unbiasing
+yhat = f(popt,x);
+SSE = sum((y-yhat) .^ 2);               % sum of squares (residual error)
+MSE = SSE ./ df;                        % mean square (residual error)
+syx = sqrt(MSE);                        % std error of the estimate
+beta_hat = popt;               			% reference optimum parameters
+y_hat_ref = f(beta_hat, x);    			% reference predicted y_hat at x
+f_new = f(beta_hat,x_new);     			% reference predicted y_new at x_new
+% - - -
+% Monte Carlo simulation
+res_f_hat = zeros(trials,n_new);
+res_y_hat = zeros(trials,n_new);
+res_popt_b = zeros(trials,nparam);
+disp('Running bootstrap Monte Carlo iterations...');
+for i=1:trials
+	y_b = y_hat_ref + syx .* normrnd(0,1,[1,nobs]);
+	% popt_b, pcov_b = opt.curve_fit(f_scipy, x, y_b, p0=popt, bounds=(-np.inf,np.inf))
+	popt_b = nlinfit(x,y_b,f,popt);
+	f_b = f(popt_b, x_new);
+	res_popt_b(i,:) = popt_b;
+	res_f_hat(i,:) = f_b;
+	res_y_hat(i,:) = f_b + normrnd(0,syx,[1,1]);
+end
+disp('Parametric bootstrap is completed');
+% - - -
+% Monte Carlo results summary
+% mc_x = x_new
+% mc_f = f_new
+% un-biased quantiles for confidence intervals and prediction intervals
+mc_lwr_conf = f_new + rq .* (quantile(res_f_hat, alpha/2, 1) - f_new);
+mc_upr_conf = f_new + rq .* (quantile(res_f_hat, 1-alpha/2, 1) - f_new);
+mc_lwr_pred = f_new + rq .* (quantile(res_y_hat, alpha/2, 1) - f_new);
+mc_upr_pred = f_new + rq .* (quantile(res_y_hat, 1-alpha/2, 1) - f_new);
+% un-biased quantiles for confidence intervals for parameters
+mc_popt_lwr_conf = beta_hat + rq .* (quantile(res_popt_b, alpha/2, 1) - beta_hat);
+mc_popt_upr_conf = beta_hat + rq .* (quantile(res_popt_b, 1-alpha/2, 1) - beta_hat);
+% - - -
+% optional additional outputs of regression statistics
+SST = sum(y .^ 2) - sum(y) .^ 2 ./ nobs;  	% sum of squares (total)
+SSR = SST - SSE;                            % sum of squares (regression model)
+MSR = SSR / (length(popt)-1);              	% mean square (regression model)
+Fstat = MSR / MSE;           				% F statistic
+dfn = length(popt) - 1;    					% df numerator = degrees of freedom for model = number of model parameters - 1
+dfd = df;                    				% df denomenator = degrees of freedom of the residual = df = nobs - nparam
+% pvalue = 1-stats.f.cdf(Fstat, dfn, dfd);    % p-value of F test statistic
+pvalue = 1 - fcdf(Fstat, dfn, dfd);			% p-value of F test statistic 
+rsquared = SSR / SST;                                                        	% ordinary rsquared
+adj_rsquared = 1-(1-rsquared) .* (length(x)-1) ./ (length(x)-length(popt)-1);  	% adjusted rsquared
+% - - -
+% put result into an output structure
+result = [];
+result.popt = popt;
+result.popt_lwr_conf = mc_popt_lwr_conf;
+result.popt_upr_conf = mc_popt_upr_conf;
+result.popt_b = res_popt_b;
+result.f_hat_b = res_f_hat;
+result.y_hat_b = res_y_hat;
+result.trials = trials;
+result.alpha = alpha;
+result.x_new = x_new';
+result.y_new = y_new';
+result.lwr_conf = mc_lwr_conf';
+result.upr_conf = mc_upr_conf';
+result.lwr_pred = mc_lwr_pred';
+result.upr_pred = mc_upr_pred';
+result.x = x';
+result.y = x';
+result.yhat = yhat';
+result.nobs = nobs;
+result.nparam = nparam;
+result.SSE = SSE;
+result.MSE = MSE;
+result.syx = syx;
+result.SST = SST;
+result.SSR = SSR;
+result.MSR = MSR;
+result.Fstat = Fstat;
+result.qt = qt;
+result.df = df;
+result.dfn = dfn;
+result.dfd = dfd;
+result.pvalue = pvalue;
+result.rsquared = rsquared;
+result.adj_rsquared = adj_rsquared;
+
